d567bd114c6d9d4a4cf30c0933a44094b9d5f2c2
Jakob Jungmann <jungmann0@gmail.com>
2022-03-26 09:10:19 +0100

    soundness improvements

--- src/pcre2.ts
+++ src/pcre2.ts
@@ -17,66 +17,58 @@ export default class PCRE2 {
     }
     static create(regexp: string): Promise<PCRE2> {
         const imp_wasm = {
-        // @ts-ignore
             memory: new WebAssembly.Memory({'initial': 100}),
         };
-        // @ts-ignore
+
         const memory = new Uint8Array(imp_wasm.memory.buffer);
 
-       return load_wasm_file( {env: imp_wasm})
+       return load_wasm_file( { env: imp_wasm })
            .then(program => {
                 // @ts-ignore
-                const instance = program['instance'];
-                const exports = instance['exports'];
-                // @ts-ignore
-                const err_buf = exports.malloc(512);
-                // @ts-ignore
+                const instance = program.instance;
+                const exports = instance.exports;
+                const err_buf = exports.malloc(256);
                 const err = exports.malloc(2);
-                // @ts-ignore
                 const err_offset = exports.malloc(4);
 
                 const te = new TextEncoder();
                 const td = new TextDecoder();
                 const text_encoded = te.encode(regexp);
-                // @ts-ignore
                 const re_ptr = exports.malloc(text_encoded.length);
                 memory.set(text_encoded, re_ptr);
+
                 if (!err_buf || !err || !err_offset) {
                     return Promise.reject(new Error("Out of memory"));
                 }
 
-                // @ts-ignore
                 const re_comp_ptr = exports.pcre2_compile_8(re_ptr, text_encoded.length, 0, err, err_offset, 0);
                 if (re_comp_ptr === 0) {
-                    // @ts-ignore
-                    // todo: memory[err] might be too short
-                    const err_len = exports.pcre2_get_error_message_8(memory[err], err_buf, 512);
+                    const error_int = Buffer.from(memory.slice(err, err + 2)).readUInt16LE(0);
+                    const error_offset_int = Buffer.from(memory.slice(err_offset, err_offset + 4)).readInt32LE(0);
+                    const err_len = exports.pcre2_get_error_message_8(error_int, err_buf, 256);
                     const error = td.decode(memory.slice(err_buf, err_buf + err_len))
-                    return Promise.reject(new Error("Failed to compile regex at offset " + memory[err_offset] + ": " + error));
+                    return Promise.reject(new Error("Failed to compile regex at offset " + error_offset_int + ": " + error));
                 }
                 return new PCRE2(regexp, instance, memory, err_buf, re_comp_ptr);
             });
     }
 
     test(subject: string, flags = 0) {
-        const exports = this.instance['exports'];
+        const exports: any = this.instance.exports;
         const td = new TextDecoder();
         const te = new TextEncoder();
 
         const subj_buf = te.encode(subject)
-        // @ts-ignore
         const subj_ptr = exports.malloc(subj_buf.length)
 
         this.memory.set(subj_buf, subj_ptr);
 
-        // @ts-ignore
         const match_data = exports.pcre2_match_data_create_from_pattern_8(this.re_comp_ptr, 0);
 
         if (match_data === 0) {
             throw new Error("match_data is null");
         }
 
-        // @ts-ignore
         const rc = exports.pcre2_match_8(
             this.re_comp_ptr,                /* the compiled pattern */
             subj_ptr,                       /* the subject string */
@@ -86,13 +78,11 @@ export default class PCRE2 {
             match_data,                     /* block for storing the result */
             0);                             /* use default match context */
 
-        // @ts-ignore
         exports.free(subj_ptr);
 
         if (rc === PCRE2_ERROR_NOMATCH) {
             return false
         } else if (rc < 0) {
-            // @ts-ignore
             const err_len = exports.pcre2_get_error_message_8(rc, this.err_buf, 256);
             throw new Error(td.decode(this.memory.slice(this.err_buf, this.err_buf + err_len)));
         }
