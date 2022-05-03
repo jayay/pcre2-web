abb03bb3468aa3c3c8c586ea887305338c4b79e4
Jakob Jungmann <jungmann0@gmail.com>
2022-05-03 13:05:00 +0200

    automate release

---
+++ scripts/release.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import {default as json} from '../src/package.template.json' assert {type: "json"};
+
+if (!process.env.PACKAGE_VERSION) {
+  throw new Error('env variable PACKAGE_VERSION is not defined');
+}
+
+const json_mut = {...json};
+json_mut.version = process.env.PACKAGE_VERSION;
+
+json_mut.files = fs.readdirSync(path.join(__dirname, '../pkg'))
+    .filter(file => file !== '.gitkeep' && file !== 'package.json' && !file.includes('.spec.'))
+
+fs.writeFileSync(path.join(__dirname, '../pkg/package.json'), JSON.stringify(json_mut, null, 2), {});
